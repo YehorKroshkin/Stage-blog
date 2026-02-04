60b69b02bb335cd1c119b79b35dbe5306408f601
Yehor Kroshkin <s154643@ap.be>
2026-02-04 15:59:26 +0100

    build fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,18 +1,15 @@
-// pages/blog/page.tsx (или app/blog/page.tsx)
-import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import PostList from "@/components/PostListComponent";
 import ScrollToTop from "@/components/ScrollToTopComponent";
-import dynamic from "next/dynamic";
+import Search from "@/components/SearchComponent";
 
-// Импортируем клиентский компонент динамически, без SSR
-const Search = dynamic(() => import("@/components/SearchComponent"), { ssr: false });
-
-export default function BlogPage() {
-  return (
-    <div className="p-8 bg-[#f8f5f0] min-h-screen">
-      <Header />
-      <Search />      {/* теперь рендерится только на клиенте */}
-      <ScrollToTop />
-    </div>
-  );
+export default function Blog() {
+    return (
+        <div className="p-8 bg-[#f8f5f0] min-h-screen ">
+                <Header/>
+                <Search/>
+              <ScrollToTop/>
+            </div>
+    );
 }

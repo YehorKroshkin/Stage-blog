102ce91e78f797118326d0289cc134a283e65c73
Yehor Kroshkin <s154643@ap.be>
2026-02-04 16:06:44 +0100

    build fix 2.0

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,13 +3,16 @@ import Header from "@/components/Header";
 import PostList from "@/components/PostListComponent";
 import ScrollToTop from "@/components/ScrollToTopComponent";
 import Search from "@/components/SearchComponent";
+import { Suspense } from "react";
 
 export default function Blog() {
-    return (
-        <div className="p-8 bg-[#f8f5f0] min-h-screen ">
-                <Header/>
-                <Search/>
-              <ScrollToTop/>
-            </div>
-    );
+  return (
+    <div className="p-8 bg-[#f8f5f0] min-h-screen ">
+      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Search />
+      </Suspense>
+      <ScrollToTop />
+    </div>
+  );
 }
